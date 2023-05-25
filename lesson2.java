5746d4afad028ca35be6c9ef8460bdb62747f045
Sergey <smtishkar@mail.ru>
2023-05-25 18:06:27 +0300

    lecture heapsort implementation

---
+++ lesson2.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+
+/**
+ * lesson2
+ */
+public class lesson2 {
+
+    public static void main(String[] args) {
+
+        int[] array = { 1, 4, 64, 7, 98, 434, 656, 321 };
+        sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void sort(int[] array) {
+        // Построение кучи (перегруппируем массив)
+        for (int i = array.length / 2 - 1; i >= 0; i--)
+            heapify(array, array.length, i);
+        // Один за другим извлекаем элементы из кучи
+        for (int i = array.length - 1; i >= 0; i--) {
+            // Перемещаем текущий корень в конец
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+            // Вызываем процедуру heapif на уменьшенной куче
+            heapify(array, i, 0);
+        }
+    }
+
+    private static void heapify(int[] array, int heapSize, int rootIndex) {
+        int largest = rootIndex; // Инициализируем наибольший элемент как корень
+        int leftChild = 2 * rootIndex + 1; // левый = 2*rootIndex + 1
+        int rightChild = 2 * rootIndex + 2;
+        // правый = 2*rootIndex + 2
+        // Если левый дочерний элемент больше корня
+        if (leftChild < heapSize && array[leftChild] > array[largest])
+            largest = leftChild;
+        // Если правый дочерний элемент больше, чем самый большой элемент на данный
+        // момент
+        if (rightChild < heapSize && array[rightChild] > array[largest])
+            largest = rightChild;
+        // Если самый большой элемент не корень
+        if (largest != rootIndex) {
+            int temp = array[rootIndex];
+            array[rootIndex] = array[largest];
+            array[largest] = temp;
+            // Рекурсивно преобразуем в двоичную кучу затронутое поддерево
+            heapify(array, heapSize, largest);
+        }
+    }
+}
